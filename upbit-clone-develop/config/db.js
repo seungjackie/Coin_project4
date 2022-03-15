f53715d8351ab9f1cb315ce05a569f82245cf64e
heidi <goldenoriole34@gmail.com>
2022-03-15 22:26:38 +0900

    0315

---
+++ upbit-clone-develop/config/db.js
@@ -0,0 +1,22 @@
+// package
+const mongoose = require("mongoose");
+const config = require("config");
+// URI
+const uri = config.get("mongoURI");
+
+// Connect MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

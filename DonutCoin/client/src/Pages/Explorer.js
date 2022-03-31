7ed5e23eecfd9b5463726514bcebb516d705e527
seungjackie <qkrtmdwo28@gmail.com>
2022-04-01 07:13:17 +0900

    aadd

--- DonutCoin/client/src/Pages/Explorer.js
+++ DonutCoin/client/src/Pages/Explorer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import withSize from "../Container/withSize";
-import BlockSearch from "../Components/Explorer/Blocksearch";
+import BlockSearch from "../Components/explorer/Blocksearch";
 import Header from "../Components/Global/Header";
 import Footer from "../Components/Global/Footer";
 

39fce3cbe1a0aa2b01464e4e5610fe273c873b42
seungjackie <qkrtmdwo28@gmail.com>
2022-03-31 17:04:55 +0900

    aaaaaaaaa11

--- DonutCoin/client/src/Components/explorer/BlockSerach.js
+++ DonutCoin/client/src/Components/explorer/BlockSerach.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-// import {Bootst}
+// import {Bootst}a
 
 const Search = styled.div`
     /* .fix-bar {
